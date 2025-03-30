eaa882c2e204711902e26a8973130c8023ab9606
James <jamequcs@suramericana.com.co>
2025-03-30 16:54:48 -0500

    Gral. sincornizacion de datos con uso de linkedSignal

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,11 @@
-import { Component, inject, signal, OnInit, input } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  OnInit,
+  input,
+  linkedSignal,
+} from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
 import { Product } from '@shared/models/product.model';
@@ -12,7 +19,7 @@ import { CartService } from '@shared/services/cart.service';
 export default class ProductDetailComponent implements OnInit {
   readonly slug = input<string>();
   product = signal<Product | null>(null);
-  cover = signal('');
+  $cover = linkedSignal(() => this.product()?.images[0] ?? '');
   private productService = inject(ProductService);
   private cartService = inject(CartService);
 
@@ -22,16 +29,13 @@ export default class ProductDetailComponent implements OnInit {
       this.productService.getOneBySlug(slug).subscribe({
         next: (product) => {
           this.product.set(product);
-          if (product.images.length > 0) {
-            this.cover.set(product.images[0]);
-          }
         },
       });
     }
   }
 
   changeCover(newImg: string) {
-    this.cover.set(newImg);
+    this.$cover.set(newImg);
   }
 
   addToCart() {
